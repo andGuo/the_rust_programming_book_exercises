158ca98735ff2b84b4f17519e660dcf1c6fa788f
Andrew Guo <andrew.c.guo@gmail.com>
2022-03-09 00:02:15 -0500

    Guessing game with game loop and panick

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Number guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // type case string to int
+        let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Guess higher"),
+            Ordering::Greater => println!("Guess lower"),
+            Ordering::Equal => { println!("You won!"); break; }
+        };
+    }
 }
